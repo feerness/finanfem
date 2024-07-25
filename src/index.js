1f05111401e596d906b5cb73d50ebad60a1e5181
babyrina <cattalinnac@gmail.com>
2024-07-24 21:59:49 -0400

    agregar funcion valores

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Herramientas from './Herramientas-html/herramientas';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Herramientas from "./Herramientas-html/herramientas";
+import Valores from "./Herramientas-html/Valores";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
-    <Herramientas/>
+    <Valores />
+    <Herramientas />
   </React.StrictMode>
 );
 
