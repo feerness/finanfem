a1fb75ac440588539ed55bc132459d34ac37e940
feerness <fernandafarinavaldes@gmail.com>
2024-07-25 20:47:38 -0400

    Agregar Api y Navbar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from './home-html/Navbar';
+import Herramientas from './Herramientas-html/herramientas'; // Asegúrate de que el nombre del archivo coincida con la importación
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/herramientas" element={<Herramientas />} />
+        {/* Agrega otras rutas aquí si es necesario */}
+      </Routes>
+    </Router>
   );
 }
 
 export default App;
+
