0109fc0c2c555315a0527b3898451aa7d4c8883b
babyrina <cattalinnac@gmail.com>
2024-08-01 16:59:13 -0400

    Agregar index.js

--- src/index.js
+++ src/index.js
@@ -6,13 +6,16 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import Herramientas from "./Herramientas-html/herramientas";
 import Valores from "./Herramientas-html/Valores";
 import App from "./App"; // Importa el componente App
+import MyFooter from "./Herramientas-html/Footer";
+import "@fortawesome/fontawesome-free/css/all.min.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
+    <App />
     <Valores />
     <Herramientas />
-    <App />
+    <MyFooter />
   </React.StrictMode>
 );
 
