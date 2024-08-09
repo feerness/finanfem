1ed55634cdfb2a56b5041fcc0aa51989ba6687f9
babyrina <cattalinnac@gmail.com>
2024-08-09 09:50:39 -0400

    arreglo homeadmin

--- src/AppAdmin.js
+++ src/AppAdmin.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from "./home-html/Navbar";
-import Herramientas from "./Herramientas-html/herramientas"; // Asegúrate de que el nombre del archivo coincida con la importación
+import Herramientas from "./Herramientas-html/herramientas";
 import ArticulosAdmin from "./recursos-admin/articulosAdmin";
 import PdfAdmin from "./recursos-admin/pdfAdmin";
 import VideosAdmin from "./recursos-admin/videosAdmin";
@@ -13,13 +13,16 @@ function App() {
     <Router>
       <Navbar />
       <Routes>
+        <Route path="/" element={<HomeAdmin />} />{" "}
+        {/* Ruta raíz para HomeAdmin */}
         <Route path="/herramientas" element={<Herramientas />} />
         <Route path="/articulosAdmin" element={<ArticulosAdmin />} />
         <Route path="/pdfAdmin" element={<PdfAdmin />} />
         <Route path="/videosAdmin" element={<VideosAdmin />} />
         <Route path="/noticiasCliente" element={<NewsComponent />} />
+        <Route path="/inicio" element={<HomeAdmin />} />{" "}
+        {/* Ruta específica para HomeAdmin, opcional si necesitas acceder a ella directamente */}
       </Routes>
-      <HomeAdmin />
     </Router>
   );
 }
