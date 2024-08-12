129dcf86bfec04c78a3779d1c12104a241acaa5f
ConiNejo <conimalebran@gmail.com>
2024-08-11 20:13:59 -0400

    un error raro, ya se soluciono

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
-<<<<<<< HEAD
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Herramientas from './Herramientas-html/herramientas';
-=======
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
@@ -16,26 +14,19 @@ import MyFooter from "./Herramientas-html/Footer";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import "./App.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
-<<<<<<< HEAD
 import AppCliente from "./AppClient";
->>>>>>> main
-=======
+
 //import AppCliente from "./AppClient";
->>>>>>> main
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-<<<<<<< HEAD
-<<<<<<< HEAD
     <Herramientas/>
-=======
     <AppCliente />
-=======
+
     <App />
->>>>>>> main
     <MyFooter />
->>>>>>> main
   </React.StrictMode>
 );
 
